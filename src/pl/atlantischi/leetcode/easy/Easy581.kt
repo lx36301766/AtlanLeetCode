f9eed66c03f0f87d0ff312892d2abb73d866edb4
lx364301766@gmail.com <lx364301766@gmail.com>
2019-10-18 02:14:32 +0800

    add

--- src/pl/atlantischi/leetcode/easy/Easy581.kt
+++ src/pl/atlantischi/leetcode/easy/Easy581.kt
@@ -15,10 +15,43 @@ package pl.atlantischi.leetcode.easy
     Then length of the input array is in range [1, 10,000].
     The input array may contain duplicates, so ascending order here means <=.
 
+
+    给定一个整数数组，你需要寻找一个连续的子数组，如果对这个子数组进行升序排序，那么整个数组都会变为升序排序。
+
+    你找到的子数组应是最短的，请输出它的长度。
+
+    示例 1:
+
+    输入: [2, 6, 4, 8, 10, 9, 15]
+    输出: 5
+    解释: 你只需要对 [6, 4, 8, 10, 9] 进行升序排序，那么整个表都会变为升序排序。
+    说明 :
+
+    输入的数组长度范围在 [1, 10,000]。
+    输入的数组可能包含重复元素 ，所以升序的意思是<=。
+
+    来源：力扣（LeetCode）
+    链接：https://leetcode-cn.com/problems/shortest-unsorted-continuous-subarray
+    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 
 //Time complexity : O(n)O(n). Four O(n)O(n) loops are used.
 //Space complexity : O(1)O(1). Constant space is used.
+
+//这个算法背后的思想是无序子数组中最小元素的正确位置可以决定左边界，最大元素的正确位置可以决定右边界。
+//
+//因此，首先我们需要找到原数组在哪个位置开始不是升序的。我们从头开始遍历数组，一旦遇到降序的元素，我们记录最小元素为 minmin 。
+//
+//类似的，我们逆序扫描数组 numsnums，当数组出现升序的时候，我们记录最大元素为 maxmax。
+//
+//然后，我们再次遍历 numsnums 数组并通过与其他元素进行比较，来找到 minmin 和 maxmax 在原数组中的正确位置。我们只需要从头开始找到第一个大于 minmin 的元素，从尾开始找到第一个小于 maxmax 的元素，它们之间就是最短无序子数组。
+//
+//我们可以再次使用下图作为说明：
+//
+//
+//
+//我们观察到指针 bb 在下标 0 以后，标记着无序子数组的左边界，指针 aa 在下标 7 以前，标记着无序子数组的右边界
+
 fun findUnsortedSubarray(nums: IntArray): Int {
     var min = Integer.MAX_VALUE
     var max = Integer.MIN_VALUE
--- src/pl/atlantischi/leetcode/easy/Easy617.kt
+++ src/pl/atlantischi/leetcode/easy/Easy617.kt
@@ -32,6 +32,16 @@ import pl.atlantischi.base.printTree
 
     Note: The merging process must start from the root nodes of both trees.
 
+
+    给定两个二叉树，想象当你将它们中的一个覆盖到另一个上时，两个二叉树的一些节点便会重叠。
+
+    你需要将他们合并为一个新的二叉树。合并的规则是如果两个节点重叠，那么将他们的值相加作为节点合并后的新值，否则不为 NULL 的节点将直接作为新二叉树的节点。
+
+    注意: 合并必须从两个树的根节点开始。
+
+    来源：力扣（LeetCode）
+    链接：https://leetcode-cn.com/problems/merge-two-binary-trees
+    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 
 fun mergeTrees(t1: TreeNode?, t2: TreeNode?): TreeNode? {
