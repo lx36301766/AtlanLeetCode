afbaad28b78ca6fb5a2900fd4ff54f8dc78dccc0
lx364301766@gmail.com <lx364301766@gmail.com>
2019-10-21 01:46:46 +0800

    refactor

--- src/pl/atlantischi/KtTest.kt
+++ src/pl/atlantischi/KtTest.kt
@@ -1,7 +1,8 @@
-package pl.atlantischi.linked_list
+package pl.atlantischi
 
 import pl.atlantischi.base.ListNode
 import pl.atlantischi.base.printNode
+import pl.atlantischi.leetcode.medium.Medium61
 
 /**
  * Created on 2019/10/21.
@@ -24,6 +25,6 @@ fun main(args: Array<String>) {
 
     var s2 = ListNode(1)
 
-    var listNode = RotateRightNode().rotateRight(s2, 0)
+    var listNode = Medium61().rotateRight(s2, 0)
     listNode.printNode()
 }
